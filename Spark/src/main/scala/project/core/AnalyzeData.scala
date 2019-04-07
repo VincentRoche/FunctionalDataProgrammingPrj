22445de60b52090bbe7b7b755c478a27550154ed
Kilian Poulin <kilian.poulin@efrei.net>
2019-04-07 16:00:11 +0200

    First query

--- Spark/src/main/scala/project/core/AnalyzeData.scala
+++ Spark/src/main/scala/project/core/AnalyzeData.scala
@@ -26,4 +26,12 @@ object AnalyzeData extends App {
 
   loadData().collect().foreach(println)
 
+  /* Number of nuclear flying buses in the northern hemisphere */
+  println("Number of nuclear flying buses in the northern hemisphere : ")
+  //loadData().filter(_.country.northHemisphere == false).count()
+
+  println(" => List of nuclear flying buses in the northern hemisphere : ")
+  loadData().groupBy(_.country.northHemisphere == false).foreach(println)
+
+  /* Number of nuclear flying buses in the southern hemisphere */
 }
