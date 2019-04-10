632dc31bd439c686ec198298536d958b6c93524c
Kilian Poulin <kilian.poulin@efrei.net>
2019-04-10 22:24:39 +0200

    Better display with Spark

--- Spark/src/main/scala/project/core/AnalyzeData.scala
+++ Spark/src/main/scala/project/core/AnalyzeData.scala
@@ -3,15 +3,22 @@ import com.tp.spark.utils.FlyingBus
 import com.tp.spark.utils.FlyingBus.Bus
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
 object AnalyzeData extends App {
 
   val pathToFile = "../nuclear-flying-buses.json"
 
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+
+
   /**
     *  Load the data from the json file and return an RDD of Bus
     */
   def loadData(): RDD[Bus] = {
+    // create spark configuration and spark context
     // create spark configuration and spark context
     val conf = new SparkConf()
       .setAppName("Flying Bus data mining")
@@ -26,7 +33,7 @@ object AnalyzeData extends App {
 
   /* Print all data */
 
-  loadData().collect().foreach(println)
+  // loadData().collect().foreach(println)
 
   /* ***********************************************************************
   ************************************************************************
@@ -43,12 +50,19 @@ object AnalyzeData extends App {
   println(" -------- Is there more broken buses when the weather is hot or cold ? -------- ")
   loadData().flatMap{case t if (t.weather == "hot" || t.weather == "cold") && t.broken == true => Some(t) case t =>  None}.filter(_ != None).groupBy(_.weather).mapValues(_.size).foreach(println)
 
+  println("\n")
+
   // Is there more broken buses in the northern or the souther hemisphere ?
-  println(" -------- Is there more broken buses when the weather is hot or cold ? -------- ")
+  println(" -------- Is there more broken buses in the northern hemisphere ? -------- ")
+  //loadData().groupBy(_.country.northHemisphere).mapValues(_.size).foreach(println)
   loadData().groupBy(_.country.northHemisphere).mapValues(_.size).foreach(println)
+  // loadData().groupBy(record => (record.busId, record.country.northHemisphere)).mapValues(_.size).foreach(println)
+
+  println("\n")
 
   // Amoung the broken buses, how many are because of an empty fuel tank
-  println(" => Amoung the broken buses, how many are because of an empty fuel tank : " + loadData().filter(tuple => (tuple.broken == false && tuple.fuel == 0)).count())
+  println(" -------- Amoung the broken buses, how many are because of an empty fuel tank ? -------- ")
+  println(" => Number : " + loadData().filter(tuple => (tuple.broken == false && tuple.fuel == 0)).count())
 
   /* ***********************************************************************
   ************************************************************************
